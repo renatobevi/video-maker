9f5baec03995eee02d3e978d11f3d3d3ff2f20af
Joao Bevilacqua <renatobevi@gmail.com>
2020-05-10 18:03:18 -0300

    feat: add search term prefix

---
+++ index.js
@@ -0,0 +1,27 @@
+const readline = require('readline-sync')
+
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+        //return 'TERM EXAMPLE'
+        return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes)
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        //console.log(selectedPrefixIndex)
+        //console.log(selectedPrefixText)
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
