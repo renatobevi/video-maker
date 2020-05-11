b72dcd88692170158c319d627d264cae22808532
Joao Bevilacqua <renatobevi@gmail.com>
2020-05-11 15:13:04 -0300

    feat: add new robots and watson AI

--- credentials/README.md
+++ credentials/README.md
@@ -8,3 +8,16 @@ File: 'algorithmia.json'
     "apiKey": "simtbTkfv/Gvj91Df2FaeuYQnIJ1"
 }
 ```
+
+## Watson Natural Language Understanding
+File: 'watson-nlu.json'
+```
+{
+  "apikey": "YFeRIxx3ktiJCcJlo3QShHYURbKNdTBqqoKM9gIuyezn",
+  "iam_apikey_description": "Auto-generated for key 7c8a4245-7446-4dc0-b08a-501a97b71800",
+  "iam_apikey_name": "Auto-generated service credentials",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/ad05b9ce2fba4aa1975dfc37ca33afce::serviceid:ServiceId-969d92cc-ae84-4b30-bbfc-b640e7854271",
+  "url": "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/e922cfd1-27e1-4b81-9047-cce1fec1ebbc"
+}
+```
--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-const readline = require('readline-sync')
 const robots = {
-    text: require('./robots/text.js')
+    input: require('./robots/input.js'),
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js'),
+    video: require('./robots/video.js'),
+    youtube: require('./robots/youtube.js')
 }
 
 async function start() {
-    const content = {}
-
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-
-    await robots.text(content)
-
-    function askAndReturnSearchTerm() {
-        //return 'TERM EXAMPLE'
-        return readline.question('Type a Wikipedia search term: ')
-    }
-
-    function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes)
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-        //console.log(selectedPrefixIndex)
-        //console.log(selectedPrefixText)
-        return selectedPrefixText
-    }
-
-    console.log(content)
+    robots.input()
+    await robots.text()
+    await robots.image()
+    await robots.video()
+    await robots.youtube()
 }
 
 start()
